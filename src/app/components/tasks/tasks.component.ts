8a11dc3586be6c7e5a488d3cc5dfae17f1862414
subbota_subbota <151066763+HandeHan@users.noreply.github.com>
2026-03-12 00:48:04 -0300

    Alteração e código

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+
+export class TasksComponent {
+
+  tasks: any[] = [];
+  newTask = '';
+
+  addTask() {
+    if (this.newTask) {
+      this.tasks.push({
+        title: this.newTask,
+        completed: false
+      });
+      this.newTask = '';
+    }
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+
+  toggleTask(task: any) {
+    task.completed = !task.completed;
+  }
+
+}
